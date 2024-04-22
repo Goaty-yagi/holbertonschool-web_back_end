fe6ddf72d94c9dc7960b01550cd84746f07b4bb2
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-04-22 14:06:00 +1000

    feat(task 5): add sum_list function with annotation

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module provides sum_list function
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    This function receive a float list and return
+    sum of them.
+    """
+    return sum(input_list)
