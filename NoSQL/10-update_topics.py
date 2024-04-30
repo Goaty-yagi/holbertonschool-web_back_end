b95fe62b5f90ee449a3d9575a206bb7f18ec700f
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-04-30 21:02:27 +1000

    chore: add name update functionality

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -9,5 +9,5 @@ def update_topics(mongo_collection, name, topics):
     changes all topics of a school document based on the name
     """
     query = {'name': name}
-    new_values = {'$set': {'topics': topics}}
-    result = mongo_collection.update_one(query, new_values)
+    topics = {'$set': {'name': name, 'topics': topics}}
+    result = mongo_collection.update_one(query, topics)
