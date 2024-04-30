2d90f457ecd74dec6afa19b81bb720fce1708545
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-04-30 14:49:02 +1000

    feat(task 8): add Python function that lists all documents in a collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module provides 
+"""
+def list_all(mongo_collection):
+    documents = mongo_collection.find()
+ 
+    # Convert the cursor to a list of dictionaries
+    document_list = list(documents)
+    return document_list
