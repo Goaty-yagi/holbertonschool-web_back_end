3f3141366f472fe284cb89a904813e856f7d2fa9
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-04-30 15:31:10 +1000

    chore(checker): add annotation

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,8 +3,10 @@
 This module provides
 """
 
+from pymongo.collection import Collection
 
-def list_all(mongo_collection):
+
+def list_all(mongo_collection: Collection) -> list:
     """
     Lists all documents in a collection:
     """
