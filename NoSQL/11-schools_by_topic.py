d9a726b4f26f527ed95f7a715b154f4699e87247
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-01 12:41:46 +1000

    docs: add function doc

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -6,7 +6,7 @@ This module provides schools_by_topic function
 
 def schools_by_topic(mongo_collection, topic):
     """
-    Lists all documents in a collection:
+    Returns the list of school having a specific topic:
     """
     query = {"topics": topic}
     result = mongo_collection.find(query)
