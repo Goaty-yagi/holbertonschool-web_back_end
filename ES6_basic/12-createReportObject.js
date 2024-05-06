425e943e2b67370ce86c77027e36477dc3389827
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-06 22:37:28 +1000

    fix: getNumberOfDepartments to return length

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -4,7 +4,7 @@ export default function createReportObject(employeesList) {
       ...employeesList,
     },
     getNumberOfDepartments() {
-      return employeesList.length;
+      return Object.keys(employeesList).length;
     },
   };
 }
