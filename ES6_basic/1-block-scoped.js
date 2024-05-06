fc1ce7586abe0dde822fef9fd512529e85841510
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-06 16:31:37 +1000

    chore(checker): modify for ESLint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-        const task = true;
-        const task2 = false;
-    }
+  if (trueOrFalse) {
+    const task = true; // assigned a value but never used
+    const task2 = false; // assigned a value but never used
+  }
 
-    return [task, task2];
+  return [task, task2];
 }
