058d50ca4ec71f36eca0951305a030858159a232
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-08 15:15:55 +1000

    fix: task2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,18 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._lenght = length;
-    this._students = students;
-
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students) || students.some((e) => typeof e !== 'string')) {
-      throw new TypeError('Length must be a number');
-    }
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -20,6 +10,9 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
   }
 
@@ -28,6 +21,9 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = length;
   }
 
@@ -36,6 +32,9 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
+    if (!Array.isArray(students) || students.some((e) => typeof e !== 'string')) {
+      throw new TypeError('Length must be a number');
+    }
     this._students = students;
   }
 }
