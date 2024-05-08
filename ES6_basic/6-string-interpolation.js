1d1e794214d4ec927237ffa3a02b94d17c84719b
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-08 15:41:21 +1000

    fix: add return to task 2, make return val one single line

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -6,7 +6,6 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  const { income, gdp, capita } = budget;
   return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income 
-  of ${income}. As of 2015, San Francisco proper had a GDP of ${gdp}, and a GDP per capita of ${capita}.`;
+  of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
