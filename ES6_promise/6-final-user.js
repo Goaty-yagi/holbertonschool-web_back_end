0103b40161983f6050882ebd76011cf264174cfd
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-14 17:35:52 +1000

    chore: make modifications

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,9 +5,13 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((responses) => responses.map((res) => {
-    const key = res.status === 'rejected' ? 'error' : 'value';
-    const val = res.status === 'rejected' ? res.reason.message : res.value;
-    return { status: res.status, [key]: val };
-  }));
+  ]).then((responses) => {
+    responses.forEach((res) => {
+      if (res.status === 'rejected') {
+        res.value = `Error: ${res.reason.message}`;
+        delete res.reason;
+      }
+    });
+    return responses;
+  });
 }
