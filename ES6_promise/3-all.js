d4b65eb86afd1e0dac123be6853149d07cf4f493
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-14 13:20:48 +1000

    feat(task 3): add promise all functionality

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photoPromise = uploadPhoto();
+  const userPromose = createUser();
+  return Promise.all([photoPromise, userPromose])
+    .then((responses) => {
+      const photo = responses[0];
+      const user = responses[1];
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      Error('Signup system offline');
+    });
+}
